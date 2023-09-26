bb8729c3e328f9a6034a29b5bd8640db60299f4d
Mikhail Rashkovskiy <miklyx@gmail.com>
2023-09-26 19:33:31 +0200

    feat:styling + refactor + some backend for maps

--- client/apiService.js
+++ client/apiService.js
@@ -123,6 +123,7 @@ export async function updateProfile (firstName, lastName, phone, userId) {
 //TECH DEBT - FOR NOW UPDATES ALL BUT MATCHES ONLY DISTRICT - NEED DEBUG STORED FUNCTION updatepreferences() in DB
 export async function updateProfileMeta (maxPrice, minSize, district, userId) {
   try {
+    console.log(district)
     const responce = await fetch(`${URL}/profileMeta`, {
       method: "POST",
       headers: {"Content-Type" : 'application/json'},
--- client/components/Map.js
+++ client/components/Map.js
@@ -8,7 +8,9 @@ export default function Map ({ route }) {
   const {userData} = route.params;
   const [flats, setFlats] = useState([]);
   const [applied, setApplied] = useState(false);
-  const [visibleFlats, setVisibleFlats] = useState(30);
+  const [visibleFlats, setVisibleFlats] = useState(15);
+  const [selectedFlat, setSelectedFlat] = useState(null);
+
 
  useEffect(() => {
    getFlats().then(res => {
@@ -18,7 +20,12 @@ export default function Map ({ route }) {
   },[])
 
   const loadMore = () => {
-    setVisibleFlats(prevVisibleFlats => prevVisibleFlats + 30);
+    setVisibleFlats(prevVisibleFlats => prevVisibleFlats + 10);
+  };
+
+  const handleMarkerPress = (flat) => {
+    if (!selectedFlat) setSelectedFlat(flat)
+    else setSelectedFlat(null);
   };
 
   const handleApply = (flat) => {
@@ -53,7 +60,7 @@ export default function Map ({ route }) {
   };
 
   const filteredFlats = flats.filter(flat => flat.preferred === 1 && flat.longitude && flat.latitude)
-  filteredFlats.slice(0, 15)
+  filteredFlats.slice(0, 5)
   //----TECH DEBT -- too much data everuthing f*s up
   /* filteredFlats.slice(0, 15).map(flat => {
     if (!flat.latitude) {
@@ -88,65 +95,62 @@ export default function Map ({ route }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.profile}>
-        <Image
-        source={require('../assets/logo_dark.png')}
-        style={styles.banner}
-        />
-         <Text> This a map of flats for {userData.first_name}</Text>
-       </View>
-      <ScrollView>
       <MapView 
         style={styles.map} 
         initialRegion={{
-          latitude: 52.51679,
-          longitude: 13.40935,
+          latitude: 52.498570832573186, 
+          longitude: 13.406639494389717,
           latitudeDelta: 0.05,
           longitudeDelta: 0.05,
         }}>
-        {filteredFlats.map((flat, index) => (
+        {filteredFlats.slice(0, visibleFlats).map((flat, index) => (
           <Marker
           key={index}
           //TECH DEBT - SWITCH COORDINATED IN DATABASE
           coordinate={{latitude: parseFloat(flat.longitude), longitude: parseFloat(flat.latitude)}}
           title={flat.title}
           description={'Price: '+flat.price+', Address: '+flat.fulladr}
-    />
-  ))}
+          onPress={() => handleMarkerPress(flat)}
+          />
+          ))}
       </MapView>
       
-       {filteredFlats.slice(0, visibleFlats).map((flat) => (
-        <View key={flat.id} style={styles.preferredFlat}>
-          <Text>{flat.title}</Text>
-          <Text>{String(flat.price)}</Text>
-          <Text>{String(flat.size)}</Text>
-          <Text>{flat.address}</Text>
-          {!flat.applied ? (
-          <TouchableOpacity style={styles.loadMoreButton} onPress={() => handleApply(flat)}>
-            <Text>Apply?</Text>
-          </TouchableOpacity>) : (
-          <Text style={{ color: 'gray' }}>Applied</Text>
-          )
-          }
-        </View>
-      
-      ))} 
       {visibleFlats < flats.length && (
         <TouchableOpacity style={styles.loadMoreButton} onPress={loadMore}>
-          <Text style={styles.loadMoreText}>More..</Text>
+          <Text style={styles.loadMoreText}>Load more flats by your taste...</Text>
         </TouchableOpacity>
+        )} 
+
+      {selectedFlat && (
+        <View style={styles.preferredFlat}>
+          <Text style={{color:"#401F3E", fontStyle: 'italic', marginBottom:10}}>{selectedFlat.title}</Text>
+          <Text>{selectedFlat.price} €</Text>
+          <Text>{selectedFlat.size} m2</Text>
+          <Text>{selectedFlat.address}</Text>
+      
+          {!selectedFlat.applied ? (
+            <TouchableOpacity style={styles.loadMoreButton} onPress={() => handleApply(selectedFlat)}>
+            <Text style={{fontWeight:'bold', color: '#401F3E', textAlign: 'right'}}>Apply to flat</Text>
+            </TouchableOpacity>
+            ) : (
+            <Text style={{fontWeight:'light', color: '#401F3E', textAlign: 'right'}}>Applied</Text>
+            )
+          } 
+      </View>
       )}
-      </ScrollView>
     </View>
-  )
-}
+  )}
+      
+
+
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#759AAB',
+    backgroundColor: '#5C4B51',
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    margin: 0,
   },
   flatBlock: {
     backgroundColor: '#FAF2A1', 
@@ -159,10 +163,10 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
   },
   preferredFlat: {
-    backgroundColor: '#FAF2A1', 
+    backgroundColor: '#d9e9e5', 
     color: '#401F3E',
-    borderWidth: 1, 
-    borderColor: 'red', 
+    borderWidth: 5, 
+    borderColor: '#401F3E', 
     borderRadius: 5, 
     padding: 10, 
     marginVertical: 10,
@@ -177,11 +181,19 @@ const styles = StyleSheet.create({
     alignSelf: 'center'
   },
   banner: {
-    width: 50,
-    marginTop: 30,
-    marginLeft: 30,
-    height: 50, 
-    resizeMode: 'cover', 
-    
+    width: '100%',
+    height: 100, 
+    resizeMode: 'cover',
+  },
+  loadMoreButton: {
+    fontWeight: 'bold',
+
+  },
+  loadMoreText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#d9e9e5',
+    marginTop: 10,
+    marginBottom: 30,
   },
 });
